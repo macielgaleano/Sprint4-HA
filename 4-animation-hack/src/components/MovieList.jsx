1cb93cba58fc8e7dd7683c2d87fbfa835b10a2d0
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-26 13:28:25 -0300

    helpm me

--- 4-animation-hack/src/components/MovieList.jsx
+++ 4-animation-hack/src/components/MovieList.jsx
@@ -5,34 +5,68 @@ import "../assets/HeaderHack.scss";
 import Stars from "./Stars";
 import Movie from "./Movie";
 import { formatUrl } from "../utilities/formatUrl";
+// import ApiMovieDb from "../utilities/apiMovieDb";
 
 const MovieList = ({ textSearched }) => {
   const [countStars, setCountStars] = React.useState(2);
   const [movies, setMovies] = React.useState([]);
+  const [scrolling, setScrolling] = React.useState(false);
+  const [scrollTop, setScrollTop] = React.useState(0);
+  const [page, setPage] = React.useState(1);
 
   function getStars(countStars) {
     setCountStars(countStars);
   }
 
+  // ApiMovieDb.GetMoviesForText(formatUrl.Search(1, textSearched), textSearched);
+  // ApiMovieDb.GetMoviesForRating(formatUrl.Rating(1, countStars), countStars);
+
   useEffect(() => {
     if (formatUrl.Rating(1, countStars)) {
-      fetch(formatUrl.Rating(1, countStars))
+      fetch(formatUrl.Rating(page, countStars))
         .then((data) => data.json())
         .then((data) => {
           setMovies(data.results);
         });
     }
-  }, [countStars]);
+  }, [countStars, page]);
 
   useEffect(() => {
     if (textSearched) {
-      fetch(formatUrl.Search(1, textSearched))
+      fetch(formatUrl.Search(page, textSearched))
         .then((data) => data.json())
         .then((data) => {
           setMovies(data.results);
         });
     }
-  }, [textSearched]);
+  }, [textSearched, page]);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setScrollTop(e.target.documentElement.scrollTop);
+      setScrolling(window.innerHeight < scrollTop + 200);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    if (scrolling) {
+      setPage(page + 1);
+      if (formatUrl.Rating(page, countStars)) {
+        fetch(formatUrl.Rating(page, countStars))
+          .then((data) => data.json())
+          .then((data) => {
+            setMovies([...movies, data.results]);
+          });
+      }
+      if (scrolling && textSearched.length > 0) {
+        fetch(formatUrl.Search(page, textSearched))
+          .then((data) => data.json())
+          .then((data) => {
+            setMovies(...movies, data.results);
+          });
+      }
+    }
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop, page, scrolling, textSearched, countStars, movies]);
 
   return (
     <div className="container-fluid">
@@ -40,11 +74,12 @@ const MovieList = ({ textSearched }) => {
         <div className="col-12 d-flex justify-content-center">
           {textSearched.length === 0 && <Stars getStars={getStars}></Stars>}
         </div>
-
-        {movies.length > 0 &&
-          movies.map((item, index) => {
-            return <Movie item={item}></Movie>;
-          })}
+        <div className="row d-flex justify-content-center">
+          {movies.length > 0 &&
+            movies.map((item, index) => {
+              return <Movie item={item} key={item.id}></Movie>;
+            })}
+        </div>
       </div>
     </div>
   );
--- 4-animation-hack/src/utilities/apiMovieDb.js
+++ 4-animation-hack/src/utilities/apiMovieDb.js
@@ -1,16 +1,31 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 let ApiMovieDb = {
   GetMoviesForText: (url, textSearched) => {
+    const [movies, setMovies] = useState([]);
     useEffect(() => {
       if (textSearched) {
         fetch(url)
           .then((data) => data.json())
           .then((data) => {
-            return data.results;
+            setMovies(data.results);
+            return movies;
           });
       }
-    }, [textSearched]);
+    }, [url, textSearched]);
+  },
+  GetMoviesForRating: ({ url, countStars }) => {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+      if (url) {
+        fetch(url)
+          .then((data) => data.json())
+          .then((data) => {
+            setMovies(data.results);
+          });
+      }
+    }, [url, countStars]);
+    return movies;
   },
 };
 
