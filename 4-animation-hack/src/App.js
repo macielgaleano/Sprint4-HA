72ac34b824a3670866c6b422194d494706672eb4
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-27 14:19:18 -0300

    help me

--- 4-animation-hack/src/App.js
+++ 4-animation-hack/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import Error from "./components/Error";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import MovieDetails from "./components/MovieDetails";
@@ -8,10 +11,33 @@ import MovieDetails from "./components/MovieDetails";
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="navbar navbar-expand-lg d-flex justify-content-center fixed-top">
+              <Link to="/" className="navbar-brand text-white mr-4">
+                Anime Hack
+              </Link>
+              <Link className="nav-link text-white ml-4 mr-2" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+              <Link className="nav-link text-white ml-4 mr-2" to="/about">
+                Acerca de <span className="sr-only">(current)</span>
+              </Link>
+              <Link className="nav-link text-white ml-4 mr-2" to="/contact">
+                Contacto <span className="sr-only">(current)</span>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
       <Route path="/pelicula/:id">
         <MovieDetails></MovieDetails>
       </Route>
+      {/* <Route path="*" exact={true} component={Error} /> */}
     </Router>
   );
 }
--- 4-animation-hack/src/components/Header.jsx
+++ 4-animation-hack/src/components/Header.jsx
@@ -14,25 +14,6 @@ const headerHack = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-12 ">
-            <nav className="navbar navbar-expand-lg d-flex justify-content-between">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <a href="/" className="navbar-brand text-white">
-                Anime Hack
-              </a>
-              <a className="nav-link text-white" href="/">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </nav>
             <div className="col-12 d-flex justify-content-center flex-column align-items-center titles ">
               <h2 className="Text-cemter text-white">
                 El lugar que estabas buscando, el mejor sitio de Fukuoka
